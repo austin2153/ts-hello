c7e98ca81456508969bb5cdc390e56b79fb38f6a
Austin Campbell <austincamp@gmail.com>
2021-05-11 07:31:18 -0400

    file changes

--- main.ts
+++ main.ts
@@ -1,7 +1,32 @@
-function log(message){
-    console.log(message);
+/*
+// function example using let
+function doSomething() {
+    for (let i = 0; i < 5; i++){
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+
 }
 
-var message = 'Hello World';
+doSomething();
+*/
 
-log(message);
+/*
+// declare variable with let
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1,2,3];
+let f: any[] = [1, true, 'a', false];
+*/
+
+/*
+// enum example
+enum Color {Red = 0, Green = 1, Blue = 2};
+let backgroundColor = Color.Red
+*/
+
+let message = 'abc';
+let endsWithC = message.endsWith('c');
+
