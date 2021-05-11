599c8ab6ae5c4183ddb11e676cbb414640f378dc
Austin-manjaro <austincamp@gmail.com>
2021-05-10 21:53:24 -0400

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-function log(message) {
-    console.log(message);
-}
-var message = 'Hello World';
-log(message);
+// // arrow function example
+// // js version
+// let log = function(message) {
+//     console.log(message);
+// }
+// // arrow version
+// let doLog = (message) => console.log(message);
+// // interfaces
+// interface Point {
+//     x: number,
+//     y: number,
+//     draw: () => void
+// }
+// let drawPoint = (point: Point) => {
+//     // ....
+// }
+// Class example
+var Point = /** @class */ (function () {
+    function Point() {
+    }
+    Point.prototype.draw = function () {
+        console.log('X: ' + this.x + ', Y: ' + this.y);
+    };
+    // when a function is part of a class, its called a method
+    Point.prototype.getDistance = function (another) {
+        // ...
+    };
+    return Point;
+}());
+// create var of type "Point"
+var point = new Point();
+point.x = 1;
+point.y = 2;
+point.draw();
--- main.ts
+++ main.ts
@@ -1,7 +1,39 @@
-function log(message){
-    console.log(message);
+
+// // arrow function example
+// // js version
+// let log = function(message) {
+//     console.log(message);
+// }
+// // arrow version
+// let doLog = (message) => console.log(message);
+
+// // interfaces
+// interface Point {
+//     x: number,
+//     y: number,
+//     draw: () => void
+// }
+
+// let drawPoint = (point: Point) => {
+//     // ....
+// }
+
+// Class example
+class Point {
+    x: number;
+    y: number;
+
+    constructor(x?: number, y?: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // When a function is part of a class, its called a method
+    draw() {
+        console.log('X: ' + this.x + ', Y: ' + this.y)
+    }
 }
 
-var message = 'Hello World';
-
-log(message);
+// create var of type "Point"
+let point = new Point(1, 2);
+point.draw();
